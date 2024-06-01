1474d0674e261d0d43a8cf7748acb6f686536965
Ali <ali.hussein.pre@gmail.com>
2024-06-01 12:41:03 +0200

    feat: add multi layer cards variants

--- src/components/cards/multi-layers.tsx
+++ src/components/cards/multi-layers.tsx
@@ -41,24 +41,47 @@ export const MultilayerCardV_1 = () => {
 };
 //======================================
 export const MultilayerCardV_2 = () => {
-
   return (
     <div className="mx-auto max-w-lg py-14">
       <div className="relative w-[26rem] h-60">
         <div
-          className="dark:bg-zinc-950 bg-white absolute size-full rounded-3xl border border-neutral-200 dark:border-zinc-800 scale-[0.95] -top-6"
+          className="dark:bg-zinc-900 bg-white absolute size-full rounded-3xl border border-neutral-200 dark:border-zinc-800 scale-y-[1.15] scale-x-90 -top-4"
           style={{
             transformOrigin: "top center",
           }}
         ></div>
         <div
-          className="dark:bg-zinc-950 bg-white absolute size-full rounded-3xl border border-neutral-200 dark:border-zinc-800 scale-[0.97] -top-3"
+          className="absolute dark:bg-zinc-950 bg-white size-full rounded-3xl p-4 shadow-xl border border-neutral-200 dark:border-zinc-800"
+          style={{
+            transformOrigin: "top center",
+          }}
+        >
+          <CardBody className="p-3" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+//======================================
+export const MultilayerCardV_3 = () => {
+  return (
+    <div className="mx-auto max-w-lg py-14">
+      <div className="relative w-[26rem] h-60">
+        <div
+          className="dark:bg-zinc-900/20 bg-white absolute size-full rounded-3xl border border-neutral-200 dark:border-zinc-800 scale-[0.95] -top-6"
           style={{
             transformOrigin: "top center",
           }}
         ></div>
         <div
-          className="absolute dark:bg-zinc-950 bg-white size-full rounded-3xl p-4 shadow-xl border border-neutral-200 dark:border-zinc-800 shadow-black/[0.1] dark:shadow-white/[0.04]"
+          className="dark:bg-zinc-900/30 bg-white absolute size-full rounded-3xl border border-neutral-200 dark:border-zinc-800 scale-[0.97] -top-3"
+          style={{
+            transformOrigin: "top center",
+          }}
+        ></div>
+        <div
+          className="absolute dark:bg-zinc-950 bg-white size-full rounded-3xl p-4 shadow-xl border border-neutral-200 dark:border-zinc-800 shadow-black/[0.1] dark:shadow-white/[0.02]"
           style={{
             transformOrigin: "top center",
           }}
