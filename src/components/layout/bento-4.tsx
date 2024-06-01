2688619efaad7d5e5d17d1235a59c10615710a1e
Ali <ali.hussein.pre@gmail.com>
2024-06-01 15:36:43 +0200

    feat: add layout bento grid variants

---
+++ src/components/layout/bento-4.tsx
@@ -0,0 +1,102 @@
+import { cn } from "@/src/utils/cn"
+
+export const Cell = ({ i = 0 }) => {
+  return (
+    <div className="size-full rounded-lg center bg-zinc-200 dark:bg-zinc-900 text-xl">
+      {i}
+    </div>
+  )
+}
+const cells = [1, 2, 3, 4]
+//======================================
+export const Bento_4_v1 = () => {
+  return (
+    <div>
+      <div className="grid md:grid-cols-4 gap-2">
+        {cells.map((n, i) => (
+          <div
+            key={n}
+            className={cn(
+              "p-1 rounded-lg h-32",
+              i == 0 && "md:col-span-3",
+              i == 2 && "md:col-start-1",
+              i == 3 && "md:col-start-2 md:col-span-4"
+            )}
+          >
+            <Cell i={i + 1} />
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+//======================================
+export const Bento_4_v2 = () => {
+  return (
+    <div>
+      <div className="grid md:grid-cols-4 gap-2">
+        {cells.map((n, i) => (
+          <div
+            key={n}
+            className={cn(
+              "p-1 rounded-lg h-32",
+              i == 0 && "md:col-span-3",
+              i > 1 && "md:col-span-2"
+            )}
+          >
+            <Cell i={i + 1} />
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+//======================================
+export const Bento_4_v3 = () => {
+  return (
+    <div>
+      <div className="grid md:grid-cols-4 gap-2">
+        {cells.map((n, i) => (
+          <div
+            key={n}
+            className={cn(
+              "p-1 rounded-lg h-32",
+              i == 0 && "md:col-start-1 ",
+              i == 1 && "md:col-span-2",
+              i == 2 && "md:col-start-4",
+              i == 3 && "md:col-span-4"
+            )}
+          >
+            <Cell i={i + 1} />
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+//======================================
+export const Bento_4_v4 = () => {
+  return (
+    <div>
+      <div className="grid md:grid-cols-4 gap-2">
+        {cells.map((n, i) => (
+          <div
+            key={n}
+            className={cn(
+              "p-1 rounded-lg h-32",
+              i == 0 && "md:col-span-3 row-span-4 h-full",
+              i == 1 && "md:col-start-4",
+              i == 2 && "md:col-start-4",
+              i == 3 && "md:col-start-4"
+            )}
+          >
+            <Cell i={i + 1} />
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
