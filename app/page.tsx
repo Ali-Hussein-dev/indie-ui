03c3419d209a886643fedd3555b794f83ac09c37
Ali <ali.hussein.pre@gmail.com>
2024-06-02 14:02:40 +0200

    refactor: use the right tags

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,10 @@ import Link from 'next/link';
 export default function HomePage() {
   return (
     <main className="flex h-screen flex-col justify-center text-center">
-      <h1 className="mb-4 text-2xl font-bold">Indie UI</h1>
-      <p className="text-xl sm:text-3xl md:text-4xl mb-3 font-semibold">
+      <span className="mb-4 text-2xl font-bold">Indie UI</span>
+      <h1 className="text-xl sm:text-3xl md:text-4xl mb-3 font-semibold">
         Cool UI components for free <br />
-      </p>
+      </h1>
       <Link href="/docs" className="text-lg font-semibold">
         Start Exploring
       </Link>
