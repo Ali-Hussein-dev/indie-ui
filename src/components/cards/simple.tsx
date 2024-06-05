300fd067146e4b38079ca39014895769f261e977
Ali <ali.hussein.pre@gmail.com>
2024-06-05 20:48:55 +0200

    style: tweak cards v1,v2

--- src/components/cards/simple.tsx
+++ src/components/cards/simple.tsx
@@ -32,17 +32,17 @@ export const SimpleCard_V1 = () => {
     )
   }
   const Container = ({ children }: { children: React.ReactNode }) => (
-    <div className="relative mx-auto w-full rounded-lg border border-dashed border-gray-300 px-4 dark:border-gray-800 sm:px-6 md:px-8">
-      <div className="absolute left-0 top-4 -z-0 h-px w-full bg-gray-400 dark:bg-gray-700 sm:top-6 md:top-8"></div>
-      <div className="absolute bottom-4 left-0 z-0 h-px w-full bg-gray-400 dark:bg-gray-700 sm:bottom-6 md:bottom-8"></div>
-      <div className="relative w-full border-x border-gray-400 dark:border-gray-700">
+    <div className="relative mx-auto w-full rounded-lg border border-dashed border-zinc-300 px-4 dark:border-zinc-800 sm:px-6 md:px-8">
+      <div className="absolute left-0 top-4 -z-0 h-px w-full bg-zinc-400 dark:bg-zinc-700 sm:top-6 md:top-8"></div>
+      <div className="absolute bottom-4 left-0 z-0 h-px w-full bg-zinc-400 dark:bg-zinc-700 sm:bottom-6 md:bottom-8"></div>
+      <div className="relative w-full border-x border-zinc-400 dark:border-zinc-700">
         <Ellipses />
-        <div className="relative z-20 mx-auto py-12">{children}</div>
+        <div className="relative z-20 mx-auto py-8">{children}</div>
       </div>
     </div>
   )
   return (
-    <div className="mx-auto w-fit max-w-lg py-4">
+    <div className="mx-auto w-fit max-w-lg">
       <Container>
         <div className="p-3 w-full center">
           <CardBody />
@@ -53,27 +53,12 @@ export const SimpleCard_V1 = () => {
 }
 //======================================
 export const SimpleCard_V2 = () => {
-  const Ellipses = () => {
-    const sharedClasses =
-      "rounded-full outline outline-8 dark:outline-gray-950 sm:my-6 md:my-8 size-1 my-4 outline-gray-50 bg-green-400"
-    return (
-      <div className="absolute z-0 grid h-full w-full items-center gap-8 lg:grid-cols-2">
-        <section className="absolute z-0 grid h-full w-full grid-cols-2 place-content-between">
-          <div className={`${sharedClasses} -mx-[2.5px]`}></div>
-          <div className={`${sharedClasses} -mx-[2px] place-self-end`}></div>
-          <div className={`${sharedClasses} -mx-[2.5px]`}></div>
-          <div className={`${sharedClasses} -mx-[2px] place-self-end`}></div>
-        </section>
-      </div>
-    )
-  }
   const Container = ({ children }: { children: React.ReactNode }) => (
     <div className="relative mx-auto w-full px-4 sm:px-6 md:px-8">
-      <div className="absolute left-0 top-4 -z-0 h-px w-full bg-gray-400 dark:bg-gray-700 sm:top-6 md:top-8"></div>
-      <div className="absolute bottom-4 left-0 z-0 h-px w-full bg-gray-400 dark:bg-gray-700 sm:bottom-6 md:bottom-8"></div>
-      <div className="relative w-full border-x border-gray-400 dark:border-gray-700">
-        <Ellipses />
-        <div className="relative z-20 mx-auto py-12">{children}</div>
+      <div className="absolute left-0 top-4 -z-0 h-px w-full bg-zinc-300 dark:bg-zinc-800 sm:top-6 md:top-8"></div>
+      <div className="absolute bottom-4 left-0 z-0 h-px w-full bg-zinc-300 dark:bg-zinc-800 sm:bottom-6 md:bottom-8"></div>
+      <div className="relative w-full border-x border-zinc-300 dark:border-zinc-800">
+        <div className="relative z-20 mx-auto py-8">{children}</div>
       </div>
     </div>
   )
