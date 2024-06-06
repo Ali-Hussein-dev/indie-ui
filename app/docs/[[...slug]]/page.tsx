e95be54822a362a6c6fa2ff04ff4878a018ec480
Ali <ali.hussein.pre@gmail.com>
2024-06-06 23:57:39 +0200

    Docs: add edit page link

--- app/docs/[[...slug]]/page.tsx
+++ app/docs/[[...slug]]/page.tsx
@@ -2,28 +2,41 @@ import { getPage, getPages } from '@/app/source';
 import type { Metadata } from 'next';
 import { DocsPage, DocsBody } from 'fumadocs-ui/page';
 import { notFound } from 'next/navigation';
-
-export default async function Page({
-  params,
-}: {
-  params: { slug?: string[] };
-}) {
-  const page = getPage(params.slug);
+import { CiEdit } from "react-icons/ci"
+export default async function Page({ params }: { params: { slug?: string[] } }) {
+  const page = getPage(params.slug)
 
   if (page == null) {
-    notFound();
+    notFound()
   }
 
-  const MDX = page.data.exports.default;
+  const MDX = page.data.exports.default
 
   return (
-    <DocsPage toc={page.data.exports.toc}>
+    <DocsPage
+      toc={page.data.exports.toc}
+      tableOfContent={{
+        footer: (
+          <div className="flex-row-start gap-2 dark:text-zinc-300">
+            <CiEdit />
+            <a
+              href={`https://github.com/Ali-Hussein-dev/indie-ui/blob/main/content${page.url}.mdx`}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:font-semibold"
+            >
+              Edit this page
+            </a>
+          </div>
+        ),
+      }}
+    >
       <DocsBody>
         <h1>{page.data.title}</h1>
         <MDX />
       </DocsBody>
     </DocsPage>
-  );
+  )
 }
 
 export async function generateStaticParams() {
