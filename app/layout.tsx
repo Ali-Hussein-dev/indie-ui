dfaf3662853f6246d219770b26d1d532ea1df67e
Ali <ali.hussein.pre@gmail.com>
2024-06-06 19:42:45 +0200

    chore: add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,9 @@ import { Analytics } from "@vercel/analytics/react"
 const inter = Inter({
   subsets: ["latin"],
 })
-export const metadata = generateSEOTags()
+export const metadata = generateSEOTags({
+  icons: [{ rel: "icon", url: "/favicon.ico" }],
+})
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
