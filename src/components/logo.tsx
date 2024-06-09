372682d4330b8dcaef51d7decc4182cd33d2eb5f
Ali <ali.hussein.pre@gmail.com>
2024-06-09 11:41:20 +0200

    feat: create Logo component

---
+++ src/components/logo.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link"
+
+//======================================
+export const LogoLink = () => {
+  return (
+    <Link href="/" className="font-extrabold">
+      Indie UI
+    </Link>
+  )
+}
