efd06471ea0198014f1656dc324ac034676bc764
Ali <ali.hussein.pre@gmail.com>
2024-06-16 17:03:58 +0200

    feat: build the header component

---
+++ app/header-demo/page.tsx
@@ -0,0 +1,56 @@
+"use client"
+import { Button } from "@/src/components/buttons/button"
+import { HeaderDemo } from "@/src/components/headers/header-demo"
+import * as React from "react"
+
+//======================================
+const HeaderDemoPage = () => {
+  const [variant, setVariant] = React.useState<"default" | "centric">("default")
+  const [isSticky, setIsSticky] = React.useState(false)
+  return (
+    <div className="relative">
+      <HeaderDemo variant={variant} sticky={isSticky} />
+      <section className="h-screen">
+        <div className="flex-row-center py-10 gap-10">
+          <Button
+            onClick={() => setVariant("default")}
+            variant={variant == "default" ? "default" : "outline"}
+          >
+            default
+          </Button>
+          <Button
+            onClick={() => setVariant("centric")}
+            variant={variant == "centric" ? "default" : "outline"}
+          >
+            centric
+          </Button>
+          <Button
+            onClick={() => setIsSticky(!isSticky)}
+            variant={isSticky ? "default" : "outline"}
+          >
+            {isSticky ? "unstick" : "stick"}
+          </Button>
+        </div>
+        <p className="text-center pb-8">
+          Resize the window to see the mobile version
+        </p>
+      </section>
+      <section
+        id="features"
+        className="h-screen mx-auto flex-row-center border-t"
+      >
+        Features
+      </section>
+      <section
+        id="pricing"
+        className="h-screen mx-auto flex-row-center border-t"
+      >
+        pricing
+      </section>
+      <section id="faqs" className="h-screen mx-auto flex-row-center border-t">
+        FAQs
+      </section>
+    </div>
+  )
+}
+export default HeaderDemoPage
---
+++ src/components/headers/header-demo.tsx
@@ -0,0 +1,83 @@
+import { FaXTwitter } from "react-icons/fa6"
+import { FaGithub } from "react-icons/fa"
+import { LogoLink } from "@/src/components/logo"
+import { Header } from "./header"
+import { Button } from "../buttons/button"
+import Link from "next/link"
+import { ThemeToggle } from "../toggle-theme"
+
+const headerLinks = [
+  { name: "Features", href: "/header-demo/#features" },
+  { name: "Pricing", href: "/header-demo/#pricing" },
+  { name: "FAQs", href: "/header-demo/#faqs" },
+]
+const icons = [
+  {
+    name: "Twitter",
+    icon: <FaXTwitter size="14" />,
+    href: "https://x.com",
+  },
+  {
+    name: "GitHub",
+    icon: <FaGithub size="14" />,
+    href: "https://github.com",
+  },
+]
+const HeaderLink = (props: { href: string; name: string }) => {
+  return <Link href={props.href}>{props.name}</Link>
+}
+export const HeaderDemo = ({
+  variant = "default",
+  sticky,
+}: {
+  variant: "default" | "centric"
+  sticky: boolean
+}) => (
+  <Header
+    Logo={<LogoLink />}
+    variant={variant}
+    sticky={sticky}
+    desktopItems={
+      <>
+        {headerLinks.map((link, i) => (
+          <HeaderLink key={i} href={link.href} name={link.name} />
+        ))}
+        <ThemeToggle />
+      </>
+    }
+    mobileItems={({ setIsOpen }) => (
+      <>
+        {headerLinks.map((link) => (
+          <Button
+            key={link.href}
+            asChild
+            variant={"outline"}
+            className="w-full rounded-xl justify-center"
+            size="lg"
+            onClick={() => setIsOpen(false)}
+          >
+            <Link href={link.href}>{link.name}</Link>
+          </Button>
+        ))}
+        <div className="flex-row-end w-full gap-3 border-t pt-4 border-dashed">
+          {icons && (
+            <div className="flex-row-center grow gap-2">
+              {icons.map((icon) => (
+                <Button
+                  key={icon.name}
+                  size="icon"
+                  className="rounded-full"
+                  variant={"outline"}
+                  asChild
+                  onClick={() => setIsOpen(false)}
+                >
+                  <Link href={icon.href}>{icon.icon}</Link>
+                </Button>
+              ))}
+            </div>
+          )}
+        </div>
+      </>
+    )}
+  />
+)
