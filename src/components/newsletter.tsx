4e8bf48b5ac278e4946bdc6a622928e6b3994a1e
Ali <ali.hussein.pre@gmail.com>
2024-06-21 17:18:52 +0200

    feat: use card variant for newsletter

--- src/components/newsletter.tsx
+++ src/components/newsletter.tsx
@@ -1,19 +1,24 @@
 import Link from "next/link"
 import { configs } from "@/configs"
 import { Button_v6 } from "@/components/buttons/button-variants"
+import { MultilayerCardV_2 } from "./cards/multi-layers"
 
 //======================================
 export const Newsletter = () => {
   return (
-    <div className="mx-auto pb-12">
-      <p className="text-center text-lg text-pretty mb-5 font-medium dark:text-zinc-200">
-        Get notified about new components & updates
-      </p>
-      <div className="mx-auto w-fit">
-        <Button_v6>
-          <Link href={configs.urls.newsletter}>Subscribe</Link>
-        </Button_v6>{" "}
-      </div>
+    <div className="mx-auto py-8">
+      <MultilayerCardV_2>
+        <div>
+          <p className="text-center text-lg text-pretty mb-5 font-bold dark:text-zinc-200 md:text-xl lg:text-2xl tracking-tight">
+            Get notified about new components & updates
+          </p>
+          <div className="mx-auto w-fit">
+            <Button_v6>
+              <Link href={configs.urls.newsletter}>Subscribe</Link>
+            </Button_v6>{" "}
+          </div>
+        </div>
+      </MultilayerCardV_2>
     </div>
   )
 }
