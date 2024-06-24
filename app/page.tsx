70b84291f65981b6615158fa1cf3cd1ab1d8344d
Ali <ali.hussein.pre@gmail.com>
2024-06-24 20:58:17 +0200

    feat: use button variant for get code

--- app/page.tsx
+++ app/page.tsx
@@ -116,13 +116,13 @@ const VariantsCard = ({
 }) => (
   <section
     id={id}
-    className="border p-2 md:px-4 py-6 border-dashed rounded-lg shadow bg-white dark:bg-black"
+    className="border px-2 md:px-4 pb-6 pt-3 border-dashed rounded-lg shadow bg-white dark:bg-black"
   >
     <div className="flex-row-between mb-4 gap-1 border-b border-dashed py-1">
       <h3 className="text-xl font-bold text-left">{title}</h3>
-      <Link href={docUrl} className="flex-row-end gap-2">
-        Get code <FaChevronRight />
-      </Link>
+      <Button asChild variant="ghost" size="sm" rightIcon={<FaChevronRight />}>
+        <Link href={docUrl}>Get code</Link>
+      </Button>
     </div>
     <div className="w-full pt-4">{children}</div>
   </section>
