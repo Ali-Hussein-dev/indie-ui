70ea7674da616ac3c58fcc4db03d794dfac29306
prokaryotdev <152484347+prokaryotdev@users.noreply.github.com>
2024-06-23 22:49:36 -0700

    feat: add dot loader  v1

--- src/components/loaders/dot-loaders.tsx
+++ src/components/loaders/dot-loaders.tsx
@@ -1,1 +1,15 @@
+//======================================
+export const Dot_Loader_v1 = () => {
+  const dotStyle = "size-8 bg-zinc-900 rounded-full border-2 shadow-sm animate-bounce dark:bg-zinc-50"
+  return (
+    <div className="flex space-x-2 justify-center items-center">
+      {/* Animated bouncing Dots */}
+      <div className={dotStyle} style={{ animationDelay: "-0.3s" }}></div>
+      <div className={dotStyle} style={{ animationDelay: "-0.15s" }}></div>
+      <div className={dotStyle}></div>
+    </div>
+  )
+}
+
+
 
