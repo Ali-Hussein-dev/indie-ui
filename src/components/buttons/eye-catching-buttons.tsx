03b5fec9dc109464c6e0c55e353feb51a7b5aa19
Ali <ali.hussein.pre@gmail.com>
2024-06-30 14:56:46 +0200

    feat: add 2 button variants

--- src/components/buttons/eye-catching-buttons.tsx
+++ src/components/buttons/eye-catching-buttons.tsx
@@ -1,5 +1,5 @@
-"use client"
-import { type AnimationProps, motion } from "framer-motion"
+import { cn } from "@/lib/utils"
+import { Button, ButtonProps } from "./button"
 
 //======================================
 export const Button_v1 = () => {
@@ -13,55 +13,32 @@ export const Button_v1 = () => {
   )
 }
 
-//======================================
-
-const animationProps = {
-  initial: { "--x": "100%", scale: 0.8 },
-  animate: { "--x": "-100%", scale: 1 },
-  whileTap: { scale: 0.95 },
-  transition: {
-    repeat: Infinity,
-    repeatType: "loop",
-    repeatDelay: 1,
-    type: "spring",
-    stiffness: 20,
-    damping: 15,
-    mass: 2,
-    scale: {
-      type: "spring",
-      stiffness: 200,
-      damping: 5,
-      mass: 0.5,
-    },
-  },
-} as AnimationProps
-
-export const Button_v2 = ({
-  label = "Get started",
-  ...rest
-}: { label: string } & React.ButtonHTMLAttributes<HTMLButtonElement> &
-  AnimationProps) => {
+//======================================Shiny Background
+export const EyeCatchingButton_v2 = ({ ...props }: ButtonProps) => {
   return (
-    <motion.button
-      {...animationProps}
-      className="relative rounded-lg px-6 py-2 font-medium backdrop-blur-xl transition-[box-shadow] duration-300 ease-in-out hover:shadow dark:bg-[radial-gradient(circle_at_50%_0%,hsla(0,0,98%,0.1)_0%,transparent_60%)] dark:hover:shadow-[0_0_20px_hsla(0,0,98%,0.1)]"
-    >
-      <span
-        className="relative block h-full w-full text-sm uppercase tracking-wide text-[rgb(0,0,0,65%)] dark:font-light dark:text-[rgb(255,255,255,90%)]"
-        style={{
-          maskImage:
-            "linear-gradient(-75deg,hsl(var(--primary)) calc(var(--x) + 20%),transparent calc(var(--x) + 30%),hsl(var(--primary)) calc(var(--x) + 100%))",
-        }}
-      >
-        {label}
-      </span>
-      <span
-        style={{
-          mask: "linear-gradient(rgb(0,0,0), rgb(0,0,0)) content-box,linear-gradient(rgb(0,0,0), rgb(0,0,0))",
-          maskComposite: "exclude",
-        }}
-        className="absolute inset-0 z-10 block rounded-[inherit] bg-[linear-gradient(-75deg,hsla(0,0,98%,0.1)_calc(var(--x)+20%),hsl(var(--primary)/50%)_calc(var(--x)+25%),hsla(0,0,98%,0.1)_calc(var(--x)+100%))] p-px"
-      ></span>
-    </motion.button>
+    <Button
+      {...props}
+      className={cn(
+        "animate-bg-shine border-[1px] rounded-lg shadow bg-[length:200%_100%] tracking-wide",
+        "dark:bg-[linear-gradient(110deg,#09090B,45%,#27272A,55%,#09090B)] dark:text-zinc-200 dark:border-zinc-800",
+        "bg-[linear-gradient(110deg,#FFF,45%,#E4E4E7,55%,#FFF)] text-zinc-800 border-zinc-300",
+        props.className
+      )}
+    />
+  )
+}
+
+//======================================Shiny Text
+export const EyeCatchingButton_v3 = ({ ...props }: ButtonProps) => {
+  return (
+    <Button
+      {...props}
+      className={cn(
+        "border-[1px] rounded-lg bg-clip-text text-transparent dark:text-transparent animate-bg-shine bg-[length:250%_100%] shadow font-bold tracking-wide",
+        "dark:bg-[linear-gradient(110deg,#D4D4D8,45%,#27272A,55%,#D4D4D8)] dark:border-zinc-800",
+        "bg-[linear-gradient(110deg,#09090B,45%,#fff,55%,#09090B)] border-zinc-300",
+        props.className
+      )}
+    />
   )
 }
