7b6e465c3b767593aa07e6c60030418d0ab3dad4
Ali <ali.hussein.pre@gmail.com>
2024-06-30 15:24:54 +0200

    refactor: make btn reusable

--- src/components/buttons/eye-catching-buttons.tsx
+++ src/components/buttons/eye-catching-buttons.tsx
@@ -2,14 +2,18 @@ import { cn } from "@/lib/utils"
 import { Button, ButtonProps } from "./button"
 
 //======================================
-export const Button_v1 = () => {
+export const EyeCatchingButton_v1 = ({ ...props }: ButtonProps) => {
   return (
-    <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[2px] shadow-lg focus:outline-none focus:ring-1 focus:ring-zinc-400 focus:ring-offset-2 focus:ring-offset-zinc-50">
-      <span className="absolute inset-[-1000%] animate-[spin_5s_linear_infinite_reverse] dark:bg-[conic-gradient(from_90deg_at_50%_50%,#fff_0%,#000_7%)] bg-[conic-gradient(from_90deg_at_50%_50%,#000_0%,#fff_5%)]" />
-      <span className="inline-flex h-full w-44 cursor-pointer items-center justify-center rounded-full bg-primary-900 px-3 py-1 font-semibold text-zinc-800 dark:text-zinc-200 backdrop-blur-xl bg-white/30 dark:bg-zinc-900/50">
-        Get Started
-      </span>
-    </button>
+    <div className="relative overflow-hidden rounded-full dark:bg-zinc-900 bg-white shadow border dark:border-zinc-800 group border-zinc-400 p-0.5">
+      <span className="absolute inset-[-1000%] animate-[spin_5s_linear_infinite_reverse] dark:bg-[conic-gradient(from_90deg_at_50%_50%,#fff_0%,#09090B_7%)] bg-[conic-gradient(from_90deg_at_50%_50%,#000_0%,#fff_5%)] group-hover:bg-none" />
+      <Button
+        {...props}
+        className={cn(
+          "h-10 px-8 rounded-full font-semibold text-zinc-800 dark:text-zinc-200 backdrop-blur-xl bg-zinc-50 dark:bg-zinc-900",
+          props.className
+        )}
+      />
+    </div>
   )
 }
 
