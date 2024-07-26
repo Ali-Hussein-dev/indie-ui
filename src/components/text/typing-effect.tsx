f91241f2d1c4fe1a0392448d67a03a51f822eaca
Ali <ali.hussein.pre@gmail.com>
2024-07-26 21:35:18 +0200

    refactor: use Framer for animation

--- src/components/text/typing-effect.tsx
+++ src/components/text/typing-effect.tsx
@@ -1,29 +1,26 @@
 'use client';
 
 import * as React from 'react';
+import { motion, useInView } from 'framer-motion';
 
 export function TypingEffect({ text = 'Typing Effect' }: { text: string }) {
-  const [displayedText, setDisplayedText] = React.useState('');
-  const [i, setI] = React.useState(0);
-
-  React.useEffect(() => {
-    const typingEffect = setInterval(() => {
-      if (i < text.length) {
-        setDisplayedText((prevState) => prevState + text.charAt(i));
-        setI(i + 1);
-      } else {
-        clearInterval(typingEffect);
-      }
-    }, 150);
-
-    return () => {
-      clearInterval(typingEffect);
-    };
-  }, [i]);
-
+  const ref = React.useRef(null);
+  const isInView = useInView(ref, { once: true });
   return (
-    <h1 className="text-xl text-center sm:text-4xl font-bold tracking-tighter md:text-6xl md:leading-[4rem]">
-      {displayedText ? displayedText : text}
+    <h1
+      ref={ref}
+      className="text-xl text-center sm:text-4xl font-bold tracking-tighter md:text-6xl md:leading-[4rem]"
+    >
+      {text.split('').map((letter, index) => (
+        <motion.span
+          key={index}
+          initial={{ opacity: 0 }}
+          animate={isInView ? { opacity: 1 } : {}}
+          transition={{ duration: 0.2, delay: index * 0.1 }}
+        >
+          {letter}
+        </motion.span>
+      ))}
     </h1>
   );
 }
