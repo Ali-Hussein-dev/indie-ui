62cf40535fccd60e40dbc57b170c9ba053ad0283
Ali <ali.hussein.pre@gmail.com>
2024-07-28 14:08:47 +0200

    feat: show bento variants with carousel

---
+++ src/components/swiper-wrapper.tsx
@@ -0,0 +1,31 @@
+'use client';
+import { Swiper, SwiperSlide, SwiperProps } from 'swiper/react';
+import { Autoplay, Pagination } from 'swiper/modules';
+
+//======================================
+export interface CustomSwiperProps {
+  slides: React.ReactNode[];
+  swiperProps?: SwiperProps;
+}
+
+export const SwiperWrapper: React.FC<CustomSwiperProps> = (props) => {
+  const defaultSwiperProps: SwiperProps = {
+    spaceBetween: 50,
+    slidesPerView: 1,
+    // autoplay: true,
+    modules: [Autoplay, Pagination],
+    pagination: {
+      clickable: true,
+      type: 'fraction',
+      //   el: '.swiper-pagination',
+    },
+    ...props.swiperProps,
+  };
+  return (
+    <Swiper {...defaultSwiperProps} className="max-w-2xl">
+      {props.slides.map((slide, i) => (
+        <SwiperSlide key={i}>{slide}</SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
