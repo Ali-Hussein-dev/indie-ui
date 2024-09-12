630822ee04c7df057157b3e3be34ab9771a8e318
Ali <ali.hussein.pre@gmail.com>
2024-09-12 14:16:39 +0200

    style: tweak the hero section

--- src/components/homepage/hero.tsx
+++ src/components/homepage/hero.tsx
@@ -4,8 +4,8 @@ import { Button } from '@/components/buttons/button';
 import { Header } from '@/components/homepage/header';
 import { configs } from '@/configs';
 import { FaArrowRight, FaGithub } from 'react-icons/fa';
-import components from '@/constants/components.json';
-import { cn } from '@/lib/utils';
+// import components from '@/constants/components.json';
+// import { cn } from '@/lib/utils';
 
 const ProductHunt = () => (
   <a href="https://www.producthunt.com/posts/indie-ui" target="_blank">
@@ -24,13 +24,28 @@ export const Hero = () => {
   return (
     <div>
       <Header />
-      <div className="mx-auto max-w-2xl h-full lg:h-[95vh] center">
-        <div className="animate-in px-2">
-          <h1 className="text-3xl sm:text-4xl md:text-5xl text-center font-black text-balance leading-loose lg:text-6xl h1 mt-10 sm:mt-16">
-            Rich Styled UI
+      <div className="mx-auto max-w-4xl h-[85vh] lg:h-[90vh] center px-2">
+        <div className="animate-in mt-10 sm:mt-16">
+          <h1 className="text-4xl md:text-5xl text-center font-black text-balance lg:text-6xl h1 mb-4">
+            Rich Styled UI Components
           </h1>
           <p className="text-center text-pretty text-lg mb-4 dark:text-zinc-400 mx-auto">
-            Make your website stand out with minimal effort
+            Make your website stand out with minimal effort.
+            <span className="block">
+              Built with{' '}
+              <span className="dark:text-green-300 text-green-700 font-semibold">
+                Reactjs
+              </span>
+              ,{' '}
+              <span className="dark:text-green-300 text-green-700 font-semibold">
+                shadcn
+              </span>{' '}
+              and{' '}
+              <span className="dark:text-green-300 text-green-700 font-semibold">
+                Framer Motion
+              </span>{' '}
+              for animation.
+            </span>
           </p>
           <div className="flex-row-center gap-4 mx-auto max-w-fit py-4">
             <Button asChild rightIcon={<FaArrowRight />}>
@@ -42,7 +57,7 @@ export const Hero = () => {
               <Link href={configs.urls.github}>Star on GitHub</Link>
             </Button>
           </div>
-          <div className="mx-auto pt-10">
+          {/* <div className="mx-auto pt-10">
             <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-2 lg:gap-3">
               {Object.values(components).map((o, i) => (
                 <Button
@@ -56,10 +71,10 @@ export const Hero = () => {
                 </Button>
               ))}
             </div>
-          </div>
-          <div className="pt-8 pb-8">
+          </div> */}
+          {/* <div className="pt-8 pb-8">
             <ProductHunt />
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
