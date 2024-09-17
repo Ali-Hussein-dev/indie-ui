ddd1ef85cabc968a8803468830ce0af1d458e566
Ali <ali.hussein.pre@gmail.com>
2024-09-17 18:35:37 +0200

    feat: make new option for expandable-card

--- src/components/cards/expandable-card.tsx
+++ src/components/cards/expandable-card.tsx
@@ -13,8 +13,13 @@ export function ExpandableCard({
    */
   className = '',
   children,
+  /**
+   * Whether the button should be full width or not.
+   */
+  wide = false,
 }: {
   height: string;
+  wide?: boolean;
   className?: string;
   children: React.ReactNode;
 }) {
@@ -27,34 +32,37 @@ export function ExpandableCard({
       setContentHeight(contentRef.current.scrollHeight);
     }
   }, []);
-
-  return (
-    <div
-      className={cn(
-        'bg-white dark:bg-zinc-950 dark:from-zinc-950 from-white w-full border px-4 pt-8 pb-5 rounded-lg shadow',
-        className,
-      )}
-    >
-      <div className="relative overflow-hidden">
-        <div
-          ref={contentRef}
-          id="expandable-content"
-          className="transition-all duration-300 ease-in-out"
-          style={{ height: isExpanded ? `${contentHeight}px` : height }}
-        >
-          <div>{children}</div>
-        </div>
-
-        <div
-          data-expanded={isExpanded}
-          className="absolute bottom-0 inset-x-0 h-24 bg-gradient-to-t from-inherit dark:from-inherit to-transparent pointer-events-none data-[expanded=true]:opacity-0 transition-opacity duration-300 ease-in-out"
-          aria-hidden={isExpanded ? 'true' : 'false'}
-        />
+return (
+  <div
+    className={cn(
+      'bg-white dark:bg-zinc-950 dark:from-zinc-950 from-white w-full border px-4 pt-8 pb-3 rounded-lg shadow',
+      className,
+    )}
+  >
+    <div className="relative overflow-hidden bg-inherit dark:bg-inherit">
+      <div
+        ref={contentRef}
+        id="expandable-content"
+        className="transition-all duration-300 ease-in-out"
+        style={{ height: isExpanded ? `${contentHeight}px` : height }}
+      >
+        <>{children}</>
       </div>
-      <div className="bg-inherit pt-2">
+      <div
+        data-expanded={isExpanded}
+        className="absolute bottom-0 inset-x-0 h-28 bg-gradient-to-t from-inherit dark:from-inherit/50 to-transparent pointer-events-none data-[expanded=true]:opacity-0 transition-opacity duration-300 ease-in-out"
+        aria-hidden={isExpanded ? 'true' : 'false'}
+      />
+      <div
+        className={cn(
+          'mx-auto bg-inherit dark:bg-inherit',
+          wide ? 'w-full' : 'w-fit',
+          isExpanded ? 'pt-2' : 'absolute bottom-4 inset-x-0',
+        )}
+      >
         <Button
           variant="outline"
-          className="w-full bg-inherit dark:bg-inherit"
+          className="w-full bg-inherit dark:bg-inherit rounded-lg"
           onClick={() => setIsExpanded(!isExpanded)}
           aria-expanded={isExpanded}
           aria-controls="expandable-content"
@@ -63,5 +71,6 @@ export function ExpandableCard({
         </Button>
       </div>
     </div>
-  );
+  </div>
+);
 }
