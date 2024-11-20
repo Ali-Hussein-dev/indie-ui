290354155ec1a451688951173d38274b2d5b0c1d
Ali <ali.hussein.pre@gmail.com>
2024-11-20 11:38:13 +0100

    feat: use drawer for field customization

---
+++ src/form-builder/components/field-customization-view.tsx
@@ -0,0 +1,281 @@
+import * as React from 'react';
+import { useMediaQuery } from '@/hooks/use-media-query';
+import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogTitle,
+  DialogHeader,
+  DialogContent,
+  DialogTrigger,
+  // DialogDescription,
+} from '@/components/ui/dialog';
+import {
+  Drawer,
+  DrawerContent,
+  // DrawerClose,
+  // DrawerDescription,
+  // DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from '@/components/ui/drawer';
+import { FaEdit } from 'react-icons/fa';
+import { FormElement } from '../form-types';
+import { useForm } from 'react-hook-form';
+import { Form } from '@/components/ui/form';
+import { isStatic } from '../libs/utils';
+import { RenderFormElement } from './render-form-element';
+
+const inputTypes = [
+  { value: 'text', label: 'Text' },
+  { value: 'email', label: 'Email' },
+  { value: 'url', label: 'URL' },
+  { value: 'number', label: 'Number' },
+  { value: 'password', label: 'Password' },
+  { value: 'tel', label: 'Telephone' },
+];
+function FormElementOptions({
+  index,
+  editFormElement,
+  close,
+  ...formElement
+}: FormElement & {
+  index: number;
+  editFormElement: (index: number, props: FormElement) => void;
+  close: () => void;
+}) {
+  const form = useForm<FormElement>({
+    defaultValues: formElement as FormElement,
+  });
+  const { handleSubmit, getValues } = form;
+  const onSubmit = () => {
+    editFormElement(index, getValues());
+    close();
+  };
+  // const hasOptions = ['Select', 'MultiSelect'].includes(formElement.fieldType);
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="pt-3 border-t border-dashed"
+      >
+        {/* {JSON.stringify(watch(), null, 2)} {index} */}
+        {/* {JSON.stringify(formElement, null, 2)} */}
+        <div>
+          {isStatic(formElement.fieldType) ? (
+            <div className="mb-4">
+              {RenderFormElement(
+                {
+                  name: 'content',
+                  label: `Customize ${formElement.fieldType}`,
+                  fieldType: 'Input',
+                  defaultValue: formElement.content,
+                  required: true,
+                },
+                form,
+              )}
+            </div>
+          ) : (
+            <div className="flex-col-start w-full gap-3 mb-2">
+              <div className="flex-row-between gap-2 w-full">
+                {RenderFormElement(
+                  {
+                    name: 'name',
+                    label: `Customize ${formElement.name}`,
+                    fieldType: 'Input',
+                    defaultValue: formElement.name,
+                    required: true,
+                  },
+                  form,
+                )}
+                {RenderFormElement(
+                  {
+                    name: 'placeholder',
+                    label: `Customize placeholder`,
+                    fieldType: 'Input',
+                    type: 'text',
+                    required: true,
+                  } as FormElement,
+                  form,
+                )}
+              </div>
+              <div className="flex-row-between gap-2 w-full">
+                {RenderFormElement(
+                  {
+                    name: 'label',
+                    label: 'Customize label',
+                    fieldType: 'Input',
+                    type: 'text',
+                    required: true,
+                  },
+                  form,
+                )}
+                {formElement.fieldType === 'Input' &&
+                  RenderFormElement(
+                    {
+                      name: 'type',
+                      label: 'Select input type',
+                      fieldType: 'Select',
+                      options: inputTypes,
+                      required: true,
+                      placeholder: 'Select input type',
+                    },
+                    form,
+                  )}
+              </div>
+              {formElement.fieldType === 'Slider' && (
+                <div className="flex-row-between gap-3">
+                  {RenderFormElement(
+                    {
+                      name: 'min',
+                      label: 'Customize min value',
+                      fieldType: 'Input',
+                      type: 'number',
+                      defaultValue: formElement.min,
+                      required: true,
+                    },
+                    form,
+                  )}
+                  {RenderFormElement(
+                    {
+                      name: 'max',
+                      label: 'Customize max value',
+                      fieldType: 'Input',
+                      type: 'number',
+                      defaultValue: formElement.max,
+                      required: true,
+                    },
+                    form,
+                  )}
+                  {RenderFormElement(
+                    {
+                      name: 'step',
+                      label: 'Customize step value',
+                      fieldType: 'Input',
+                      type: 'number',
+                      defaultValue: formElement.step,
+                      required: true,
+                    },
+                    form,
+                  )}
+                </div>
+              )}
+              {formElement.fieldType === 'ToggleGroup' &&
+                RenderFormElement(
+                  {
+                    name: formElement.name,
+                    label: 'Select type for ToggleGroup',
+                    fieldType: 'ToggleGroup',
+                    type: 'single',
+                    options: [
+                      { value: 'single', label: 'Single' },
+                      { value: 'multiple', label: 'Multiple' },
+                    ],
+                    required: true,
+                  },
+                  form,
+                )}
+              {RenderFormElement(
+                {
+                  name: 'description',
+                  label: 'Select type for ToggleGroup',
+                  fieldType: 'Input',
+                  placeholder: 'Describe the field',
+                },
+                form,
+              )}
+              <div className="flex-row-start gap-4 pl-1">
+                {RenderFormElement(
+                  {
+                    name: 'required',
+                    label: 'Required',
+                    fieldType: 'Checkbox',
+                  },
+                  form,
+                )}
+                {RenderFormElement(
+                  {
+                    name: 'disabled',
+                    label: 'Disabled',
+                    fieldType: 'Checkbox',
+                  },
+                  form,
+                )}
+              </div>
+            </div>
+          )}
+        </div>
+        <div className="flex-row-end gap-3 w-full">
+          <Button size="sm" variant="ghost" onClick={close} type="button">
+            Cancel
+          </Button>
+          <Button size="sm" type="submit" variant="secondary">
+            Save
+          </Button>
+        </div>
+      </form>
+    </Form>
+  );
+}
+
+const OpenButton = () => (
+  <Button
+    type="button"
+    variant="ghost"
+    size="icon"
+    className="rounded-full h-10"
+  >
+    <FaEdit />
+  </Button>
+);
+export function FieldCustomizationView({
+  index,
+  editFormElement,
+  formElement,
+}: {
+  index: number;
+  formElement: FormElement;
+  editFormElement: (index: number, props: FormElement) => void;
+}) {
+  const [open, setOpen] = React.useState(false);
+  const isDesktop = useMediaQuery('(min-width: 768px)');
+  const close = () => setOpen(false);
+  const title = 'Customize form field attributes';
+  const SavedFormElementOptions = () => (
+    <FormElementOptions
+      index={index}
+      editFormElement={editFormElement}
+      {...formElement}
+      close={close}
+    />
+  );
+  if (isDesktop) {
+    return (
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger asChild>
+          <OpenButton />
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[520px]">
+          <DialogHeader>
+            <DialogTitle>{title}</DialogTitle>
+          </DialogHeader>
+          <SavedFormElementOptions />
+        </DialogContent>
+      </Dialog>
+    );
+  }
+
+  return (
+    <Drawer open={open} onOpenChange={setOpen}>
+      <DrawerTrigger asChild>
+        <OpenButton />
+      </DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader className="text-left">
+          <DrawerTitle>{title}</DrawerTitle>
+        </DrawerHeader>
+        <SavedFormElementOptions />
+      </DrawerContent>
+    </Drawer>
+  );
+}
--- src/form-builder/components/form-edit.tsx
+++ src/form-builder/components/form-edit.tsx
@@ -1,213 +1,10 @@
-'use client';
-import { FaEdit } from 'react-icons/fa';
-import { FormElement, FormFieldElement } from '../form-types';
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-} from '@/components/ui/accordion';
+import { FormElement } from '../form-types';
 import { Button } from '@/components/ui/button';
 import { MdDelete } from 'react-icons/md';
-import { cn } from '@/lib/utils';
-import { useForm } from 'react-hook-form';
 import { LuGripVertical } from 'react-icons/lu';
 import { Reorder } from 'framer-motion';
 import * as React from 'react';
-import { RenderFormElement } from './render-form-element';
-import { Form } from '@/components/ui/form';
-import { isStatic } from '../libs/utils';
-
-const inputTypes = [
-  { value: 'text', label: 'Text' },
-  { value: 'email', label: 'Email' },
-  { value: 'url', label: 'URL' },
-  { value: 'number', label: 'Number' },
-  { value: 'password', label: 'Password' },
-  { value: 'tel', label: 'Telephone' },
-];
-const FormElementOptions = ({
-  index,
-  editFormElement,
-  close,
-  ...formElement
-}: FormElement & {
-  index: number;
-  editFormElement: (index: number, props: FormElement) => void;
-  close: () => void;
-}) => {
-  const form = useForm<FormElement>({
-    defaultValues: formElement as FormElement,
-  });
-  const { handleSubmit, getValues } = form;
-  const onSubmit = () => {
-    editFormElement(index, getValues());
-    close();
-  };
-  // const hasOptions = ['Select', 'MultiSelect'].includes(formElement.fieldType);
-  /**
-   * TODO:
-   *  1- Add options to form element
-   *  3-
-   */
-  return (
-    <Form {...form}>
-      <form onSubmit={handleSubmit(onSubmit)} className="pt-1 px-5">
-        {/* {JSON.stringify(watch(), null, 2)} {index} */}
-        {/* {JSON.stringify(formElement, null, 2)} */}
-        {isStatic(formElement.fieldType) ? (
-          <div className="mb-4">
-            {RenderFormElement(
-              {
-                name: 'content',
-                label: `Customize ${formElement.fieldType}`,
-                fieldType: 'Input',
-                defaultValue: formElement.content,
-                required: true,
-              },
-              form,
-            )}
-          </div>
-        ) : (
-          <div className="flex-col-start w-full gap-3 mb-4">
-            <div className="flex-row-between gap-2 w-full">
-              {RenderFormElement(
-                {
-                  name: 'name',
-                  label: `Customize ${formElement.name}`,
-                  fieldType: 'Input',
-                  defaultValue: formElement.name,
-                  required: true,
-                },
-                form,
-              )}
-              {RenderFormElement(
-                {
-                  name: 'placeholder',
-                  label: `Customize placeholder`,
-                  fieldType: 'Input',
-                  type: 'text',
-                  required: true,
-                } as FormFieldElement,
-                form,
-              )}
-            </div>
-            <div className="flex-row-between gap-2 w-full">
-              {RenderFormElement(
-                {
-                  name: 'label',
-                  label: 'Customize label',
-                  fieldType: 'Input',
-                  type: 'text',
-                  required: true,
-                },
-                form,
-              )}
-              {formElement.fieldType === 'Input' &&
-                RenderFormElement(
-                  {
-                    name: 'type',
-                    label: 'Select input type',
-                    fieldType: 'Select',
-                    options: inputTypes,
-                    required: true,
-                    placeholder: 'Select input type',
-                  },
-                  form,
-                )}
-            </div>
-            {formElement.fieldType === 'Slider' && (
-              <div className="flex-row-between gap-3">
-                {RenderFormElement(
-                  {
-                    name: 'min',
-                    label: 'Customize min value',
-                    fieldType: 'Input',
-                    type: 'number',
-                    defaultValue: formElement.min,
-                    required: true,
-                  },
-                  form,
-                )}
-                {RenderFormElement(
-                  {
-                    name: 'max',
-                    label: 'Customize max value',
-                    fieldType: 'Input',
-                    type: 'number',
-                    defaultValue: formElement.max,
-                    required: true,
-                  },
-                  form,
-                )}
-                {RenderFormElement(
-                  {
-                    name: 'step',
-                    label: 'Customize step value',
-                    fieldType: 'Input',
-                    type: 'number',
-                    defaultValue: formElement.step,
-                    required: true,
-                  },
-                  form,
-                )}
-              </div>
-            )}
-            {formElement.fieldType === 'ToggleGroup' &&
-              RenderFormElement(
-                {
-                  name: formElement.name,
-                  label: 'Select type for ToggleGroup',
-                  fieldType: 'ToggleGroup',
-                  type: 'single',
-                  options: [
-                    { value: 'single', label: 'Single' },
-                    { value: 'multiple', label: 'Multiple' },
-                  ],
-                  required: true,
-                },
-                form,
-              )}
-            {RenderFormElement(
-              {
-                name: 'description',
-                label: 'Select type for ToggleGroup',
-                fieldType: 'Input',
-                placeholder: 'Describe the field',
-              },
-              form,
-            )}
-            <div className="flex-row-start gap-4 pl-1 pt-2">
-              {RenderFormElement(
-                {
-                  name: 'required',
-                  label: 'Required',
-                  fieldType: 'Checkbox',
-                },
-                form,
-              )}
-              {RenderFormElement(
-                {
-                  name: 'disabled',
-                  label: 'Disabled',
-                  fieldType: 'Checkbox',
-                },
-                form,
-              )}
-            </div>
-          </div>
-        )}
-        <div className="flex-row-end gap-3 w-full">
-          <Button size="sm" variant="ghost" onClick={close} type="button">
-            Cancel
-          </Button>
-          <Button size="sm" type="submit" variant="secondary">
-            Save
-          </Button>
-        </div>
-      </form>
-    </Form>
-  );
-};
+import { FieldCustomizationView } from './field-customization-view';
 
 //======================================
 export function FormEdit({
@@ -221,19 +18,8 @@ export function FormEdit({
   dropElement: (id: string) => void;
   editFormElement: (index: number, props: FormElement) => void;
 }) {
-  const [openItems, setOpenItems] = React.useState<string[]>([]);
-  const onValueChange = (value: string) => {
-    setOpenItems((prev) => (prev.includes(value) ? [] : [value]));
-  };
-  const closeAccordionItem = () => {
-    setOpenItems([]);
-  };
   return (
-    <Accordion
-      type="single"
-      collapsible
-      value={openItems.length > 0 ? openItems[0] : undefined}
-    >
+    <div>
       <Reorder.Group
         axis="y"
         onReorder={(newOrder) => {
@@ -253,15 +39,9 @@ export function FormEdit({
             className="w-full"
             // whileDrag={{ backgroundColor: '#e5e7eb', borderRadius: '12px' }}
           >
-            <AccordionItem
+            <div
               key={element.name}
-              value={element.name}
-              className={cn(
-                'rounded-xl border border-dashed py-1 bg-background',
-                // when open item style differently
-                openItems.includes(element.name) &&
-                  'dark:bg-secondary/40 bg-secondary/50 transition-colors duration-100',
-              )}
+              className="rounded-xl border border-dashed py-1 bg-background"
             >
               <div className="flex-row-between px-3">
                 <div className="flex-row-start gap-1.5 size-full">
@@ -270,14 +50,11 @@ export function FormEdit({
                 </div>
                 <div className="flex-row-end gap-1">
                   {element.fieldType !== 'Separator' && (
-                    <Button
-                      size="icon"
-                      className="rounded-full center"
-                      variant="ghost"
-                      onClick={() => onValueChange(element.name)}
-                    >
-                      <FaEdit />
-                    </Button>
+                    <FieldCustomizationView
+                      formElement={element as FormElement}
+                      editFormElement={editFormElement}
+                      index={i}
+                    />
                   )}
                   <Button
                     size="icon"
@@ -289,18 +66,10 @@ export function FormEdit({
                   </Button>
                 </div>
               </div>
-              <AccordionContent>
-                <FormElementOptions
-                  editFormElement={editFormElement}
-                  {...element}
-                  index={i}
-                  close={closeAccordionItem}
-                />
-              </AccordionContent>
-            </AccordionItem>
+            </div>
           </Reorder.Item>
         ))}
       </Reorder.Group>
-    </Accordion>
+    </div>
   );
 }
