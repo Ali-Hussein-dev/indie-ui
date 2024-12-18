36e892a4c67e648f2f90e84babccd5f7f8e159d2
Ali <ali.hussein.pre@gmail.com>
2024-12-18 19:09:47 +0100

    feat: render MS form elements in edit section

--- src/form-builder/components/form-edit.tsx
+++ src/form-builder/components/form-edit.tsx
@@ -1,4 +1,8 @@
-import { FormElement, FormElementOrList } from '@/form-builder/form-types';
+import {
+  FormElement,
+  FormElementOrList,
+  FormStep,
+} from '@/form-builder/form-types';
 import * as React from 'react';
 import { AnimatePresence, Reorder } from 'framer-motion';
 import { MdDelete } from 'react-icons/md';
@@ -8,6 +12,7 @@ import { LuGripVertical } from 'react-icons/lu';
 import { FieldCustomizationView } from '@/form-builder/components/field-customization-view';
 import { FormElementsDropdown } from '@/form-builder/components/form-elements-dropdown';
 import useFormBuilderStore from '@/form-builder/hooks/use-form-builder-store';
+import { StepContainer } from '@/form-builder/components/step-container';
 
 type EditFormItemProps = {
   element: FormElement;
@@ -19,6 +24,7 @@ type EditFormItemProps = {
    * Index of the nested array element
    */
   j?: number;
+  stepIndex?: number;
 };
 
 const EditFormItem = (props: EditFormItemProps) => {
@@ -26,7 +32,7 @@ const EditFormItem = (props: EditFormItemProps) => {
   const dropElement = useFormBuilderStore((s) => s.dropElement);
   const isNested = typeof props?.j === 'number';
   return (
-    <div className="w-full bg-background group">
+    <div className="w-full group">
       <div className="flex-row-between px-2">
         <div className="flex-row-start gap-2 size-full">
           {isNested ? (
@@ -61,8 +67,19 @@ const EditFormItem = (props: EditFormItemProps) => {
   );
 };
 
+const NoStepsPlaceholder = () => {
+  const { addFormStep } = useFormBuilderStore();
+  return (
+    <div className="flex-col-center gap-4 text-muted-foreground">
+      <Button size="sm" onClick={() => addFormStep(0)}>
+        Add first Step
+      </Button>
+    </div>
+  );
+};
 //======================================
 export function FormEdit() {
+  const isMS = useFormBuilderStore((s) => s.isMS);
   const formElements = useFormBuilderStore((s) => s.formElements);
   const reorder = useFormBuilderStore((s) => s.reorder);
 
@@ -72,77 +89,161 @@ export function FormEdit() {
     exist: { opacity: 0, y: -15 },
     transition: { duration: 0.2, type: 'spring' },
   };
-
-  return (
-    <Reorder.Group
-      axis="y"
-      onReorder={(newOrder) => {
-        reorder({ newOrder, fieldIndex: null });
-      }}
-      values={formElements}
-      className="flex flex-col gap-3"
-      tabIndex={-1}
-    >
-      <AnimatePresence mode="popLayout">
-        {formElements.map((element: FormElementOrList, i) => {
-          if (Array.isArray(element)) {
-            return (
-              <Reorder.Item
-                value={element}
-                key={element.map((f) => f.name).join('-')}
-                variants={animateVariants}
-                initial="initial"
-                animate="animate"
-                exit="exist"
-              >
-                <div className="flex items-center justify-start gap-2 ">
-                  <Button
-                    onClick={() => {
-                      reorder({ newOrder: element.reverse(), fieldIndex: i });
-                    }}
-                    variant="ghost"
-                    className="center shrink h-full py-4 border border-dashed rounded-xl bg-background px-3.5"
-                  >
-                    <IoIosSwap className="dark:text-muted-foreground text-muted-foreground" />
-                  </Button>
-                  <div className="flex items-center justify-start grow flex-wrap sm:flex-nowrap w-full gap-3">
-                    {element.map((el, j) => (
-                      <div
-                        key={el.name + j}
-                        className="w-full rounded-xl border border-dashed py-1.5"
-                      >
-                        <EditFormItem
-                          key={el.name + j}
-                          fieldIndex={i}
-                          j={j}
-                          element={el}
-                        />
-                      </div>
-                    ))}
-                  </div>
+  switch (isMS) {
+    case true:
+      if (formElements.length === 0) {
+        return <NoStepsPlaceholder />;
+      }
+      return (
+        <div className="flex flex-col gap-4 ">
+          <AnimatePresence>
+            {(formElements as FormStep[]).map((step, stepIndex) => {
+              return (
+                <div key={step.id}>
+                  <StepContainer key={stepIndex} stepIndex={stepIndex}>
+                    <Reorder.Group
+                      axis="y"
+                      onReorder={(newOrder) => {
+                        console.log({ newOrder });
+                        // reorder(newOrder);
+                      }}
+                      values={step.stepFields}
+                      className="flex flex-col gap-3"
+                      tabIndex={-1}
+                    >
+                      {step.stepFields.map((element, fieldIndex) => {
+                        if (Array.isArray(element)) {
+                          return (
+                            <div
+                              key={element.map((f) => f.name).join('-')}
+                              className="flex items-center justify-start gap-2 "
+                            >
+                              <Button
+                                onClick={() => {
+                                  // reorderHorizontal(element.reverse(), i);
+                                }}
+                                variant="ghost"
+                                className="center shrink h-full py-4 border border-dashed rounded-xl bg-background px-3.5"
+                              >
+                                <IoIosSwap className="dark:text-muted-foreground text-muted-foreground" />
+                              </Button>
+                              <div className="flex items-center justify-start grow flex-wrap sm:flex-nowrap w-full gap-2">
+                                {element.map((el, j) => (
+                                  <Reorder.Item
+                                    value={el}
+                                    key={el.name + j}
+                                    className="w-full rounded-xl border border-dashed py-1.5"
+                                  >
+                                    <EditFormItem
+                                      key={el.name + j}
+                                      fieldIndex={fieldIndex}
+                                      j={j}
+                                      element={el}
+                                      stepIndex={stepIndex}
+                                    />
+                                  </Reorder.Item>
+                                ))}
+                              </div>
+                            </div>
+                          );
+                        }
+                        return (
+                          <div
+                            key={element.name + stepIndex + fieldIndex}
+                            className="w-full rounded-xl border border-dashed py-1.5 bg-background"
+                          >
+                            <EditFormItem
+                              key={element.name + stepIndex}
+                              fieldIndex={fieldIndex}
+                              element={element}
+                              stepIndex={stepIndex}
+                            />
+                          </div>
+                        );
+                      })}
+                    </Reorder.Group>
+                  </StepContainer>
                 </div>
-              </Reorder.Item>
-            );
-          }
-          return (
-            <Reorder.Item
-              key={element.name}
-              value={element}
-              className="rounded-xl border border-dashed py-1.5 w-full"
-              variants={animateVariants}
-              initial="initial"
-              animate="animate"
-              exit="exist"
-            >
-              <EditFormItem
-                key={element.name + i}
-                fieldIndex={i}
-                element={element}
-              />
-            </Reorder.Item>
-          );
-        })}
-      </AnimatePresence>
-    </Reorder.Group>
-  );
+              );
+            })}
+          </AnimatePresence>
+        </div>
+      );
+    default:
+      return (
+        <Reorder.Group
+          axis="y"
+          onReorder={(newOrder) => {
+            reorder({ newOrder, fieldIndex: null });
+          }}
+          values={formElements as FormElementOrList[]}
+          className="flex flex-col gap-3"
+          tabIndex={-1}
+        >
+          <AnimatePresence mode="popLayout">
+            {(formElements as FormElementOrList[]).map((element, i) => {
+              if (Array.isArray(element)) {
+                return (
+                  <Reorder.Item
+                    value={element}
+                    key={element.map((f) => f.name).join('-')}
+                    variants={animateVariants}
+                    initial="initial"
+                    animate="animate"
+                    exit="exist"
+                  >
+                    <div className="flex items-center justify-start gap-2 ">
+                      <Button
+                        onClick={() => {
+                          reorder({
+                            newOrder: element.reverse(),
+                            fieldIndex: i,
+                          });
+                        }}
+                        variant="ghost"
+                        className="center shrink h-full py-4 border border-dashed rounded-xl bg-background px-3.5"
+                      >
+                        <IoIosSwap className="dark:text-muted-foreground text-muted-foreground" />
+                      </Button>
+                      <div className="flex items-center justify-start grow flex-wrap sm:flex-nowrap w-full gap-3">
+                        {element.map((el, j) => (
+                          <div
+                            key={el.name + j}
+                            className="w-full rounded-xl border border-dashed py-1.5"
+                          >
+                            <EditFormItem
+                              key={el.name + j}
+                              fieldIndex={i}
+                              j={j}
+                              element={el}
+                            />
+                          </div>
+                        ))}
+                      </div>
+                    </div>
+                  </Reorder.Item>
+                );
+              }
+              return (
+                <Reorder.Item
+                  key={element.name}
+                  value={element}
+                  className="rounded-xl border border-dashed py-1.5 w-full bg-background"
+                  variants={animateVariants}
+                  initial="initial"
+                  animate="animate"
+                  exit="exist"
+                >
+                  <EditFormItem
+                    key={element.name + i}
+                    fieldIndex={i}
+                    element={element}
+                  />
+                </Reorder.Item>
+              );
+            })}
+          </AnimatePresence>
+        </Reorder.Group>
+      );
+  }
 }
