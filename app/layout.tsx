7b53394edbba9f92f8d2e7f419ad760632c99263
Ali <ali.hussein.pre@gmail.com>
2024-12-27 16:03:17 +0100

    feat: integrate PostHog analytics provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import { generateSEOTags } from '@/lib/seo';
 import './global.css';
 import { RootProvider } from 'fumadocs-ui/provider';
-import { Inter, Poppins } from 'next/font/google';
+import { Poppins } from 'next/font/google';
 import type { ReactNode } from 'react';
 import { Banner } from '@/components/banner';
 import { AnalyticsProv } from '@/analytics-prov';
@@ -21,15 +21,14 @@ export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html
       lang="en"
-      className={'scroll-smooth antialiased ' + font.className}
+      className={`scroll-smooth antialiased ${font.className}`}
       style={{ colorScheme: 'dark' }}
     >
-      <head>
-        <AnalyticsProv />
-      </head>
       <body className="selection:bg-zinc-800 selection:text-zinc-100 relative">
         <Banner />
-        <RootProvider>{children}</RootProvider>
+        <RootProvider>
+          <AnalyticsProv>{children}</AnalyticsProv>
+        </RootProvider>
       </body>
     </html>
   );
--- src/analytics-prov.tsx
+++ src/analytics-prov.tsx
@@ -1,18 +1,18 @@
-import Script from 'next/script';
+'use client';
+import posthog from 'posthog-js';
+import { PostHogProvider } from 'posthog-js/react';
 
 const isDev = process.env.NEXT_PUBLIC_VERCEL_ENV !== 'production';
 
 //======================================
-export const AnalyticsProv = () => {
-  if (isDev) return null;
-  const ALIYTICS_SRC = process.env?.NEXT_PUBLIC_ALIYTICS_SRC;
-  const ALIYTICS_ID = process.env?.NEXT_PUBLIC_ALIYTICS_ID;
-  return (
-    <Script
-      async
-      strategy="afterInteractive"
-      src={ALIYTICS_SRC}
-      data-website-id={ALIYTICS_ID}
-    />
-  );
+export const AnalyticsProv = ({ children }: { children: React.ReactNode }) => {
+  if (isDev) return <>{children}</>;
+  if (typeof window !== 'undefined') {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
+      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+      person_profiles: 'identified_only',
+      // or 'always' to create profiles for anonymous users as well
+    });
+  }
+  return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
 };
