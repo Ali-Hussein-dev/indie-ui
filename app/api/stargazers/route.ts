5ec2dfcd45caffd0fc2f56e0dbb34796520a2254
Ali <ali.hussein.pre@gmail.com>
2025-03-11 11:49:09 +0100

    feat: make route dynamic

--- app/api/stargazers/route.ts
+++ app/api/stargazers/route.ts
@@ -1,6 +1,8 @@
 import { type NextRequest, NextResponse} from 'next/server'
 import { getStargazers } from '@/lib/get-stargazer';
 
+export const dynamic = 'force-dynamic'
+
 export const GET = async (req: NextRequest) => {
 
     const data = await getStargazers({owner: "ali-hussein-dev", repo: "indie-ui"}).catch(console.error)
